fd155d7f9b8dbae2566350ceaf289bd899d9b81f
Alex <savok92@gmail.com>
2016-08-12 11:58:25 +0300

    The complete work

---
+++ [PT2-02-16][P4].py
@@ -0,0 +1,23 @@
+myString = "text with symbols!@#$^&*( ends here"
+names = ['train', 'Train', 'Bus', 'Car', 'Airplane', 'Bike', 'Airport']
+
+def symbols(myString):
+    new_string = ''
+    for letter in myString:
+        if letter.isalnum() == True or letter == ' ':
+            new_string += letter
+        pass
+    return new_string
+
+newMyString = symbols(myString)
+
+def mylist(newMyString, names):
+    new_list = []
+    for letter in newMyString:
+        for name in names:
+            if name.startswith(letter):
+                new_list += name
+        return new_list
+
+list = mylist(newMyString, names)
+print list
