ebbbade8190ef61123f3ba2b49b903e155eacb40
Alex <savok92@gmail.com>
2016-08-29 17:51:32 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Factory.py

--- [PT2-02-16][P7]/Car.py
+++ [PT2-02-16][P7]/Car.py
@@ -1,56 +1,55 @@
 import random
 
 
-class Factory(object):
+class Fabric(object):
     cars_created = 0
 
-    def __init__(self, engine='petrol', price=10000, tank=60,):
+    def __init__(self):
+        pass
 
-        Factory.cars_created += 1
-        self.maximum_mileage = 200000
-        self.engine = engine
-        self.consumption = 8
-        self.tank = tank
+    def make_car(self):
+        cars_created = + 1
+        car = Petrol()
+        if not cars_created % 3:
+            car = Diesel()
+        if not cars_created % 5:
+            car.tank = 75
+            return car
+
+
+class Car(object):
+
+    def __init__(self, mileage, price=10000, tank=60):
         self.price = price
-        if not Factory.cars_created % 3:
-            self.engine = 'diesel'
-            self.maximum_mileage = 150000
-        if not Factory.cars_created % 5:
-            self.tank = 75
-
-car_pool = (Factory() for car in range(30))
-for car in car_pool:
-    print(car.__dict__)
-
-
-class Route(object):
-    def __init__(self, max_range=186000, min_range=29000):
-
-        self.max_range = max_range
-        self.min_range = min_range
-
-    @staticmethod
-    def distance(self):
-        return random.randrange(self.min_range, self.max_range)
-distance = Route.distance(Route())
-print distance
-
-
-class Car(Factory):
-
-    def __init__(self, mileage, depreciation):
-        super(self.__class__, self).__init__()
         self.mileage = mileage
-        self.depreciation = depreciation
+        self.tank = tank
 
-    def fill_tank(self):
-        return self.tank
+    def ride(self):
+        pass
 
-
-
-
-
+    def refill(self):
+        pass
 
 
+class Diesel(Car):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.maximum_mileage = 150000
+        self.consumption = 6
 
 
+class Petrol(Car):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.maximum_mileage = 200000
+        self.consumption = 8
+
+
+
+#def distance(self):
+#   return random.randrange(self.min_range, self.max_range)
+
+
+#car_pool = (Car() for car in range(30))
+#for car in car_pool:
+#    print(car.__dict__)
